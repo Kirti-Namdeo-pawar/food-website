21b9949a8e40dbede0cc9fa47feaf072ce41d77a
mayuri kantilal das <mayurikantilaldas@gmail.com>
2023-05-01 01:19:26 +0530

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
                                                                                                          import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { CartComponent } from './cart/cart.component';
-
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
@@ -19,7 +17,6 @@ import { HomeComponent } from './home/home.component';
     LoginComponent,
     MenuComponent,
     CartComponent,
-  
     AboutComponent,
         HomeComponent
   ],
